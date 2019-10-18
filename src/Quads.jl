26f3d52583c8c7fb32ef7a0dc1ca36a1626eccde
Higgin <Town>
2019-10-18 13:46:50 -0500

    tests

--- src/Quads.jl
+++ src/Quads.jl
@@ -1,8 +1,18 @@
 module Quads
+using Random
 
 include("PokerCard.jl")
 include("PokerDeck.jl")
 include("Lookup.jl")
 include("Evaluator.jl")
+
+
+
+PokerCard.newCard("Ah")
+
+export PokerCard
+export PokerDeck
+export Lookup
+export Evaluatorus
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,28 @@
-# using Quads
+using Quads
 using Test
+using Random
 
 import Quads.PokerCard
 import Quads.PokerDeck
 import Quads.Lookup
 import Quads.Evaluator
 
-
-
 @testset "Quads.jl" begin
-    nothing
-end
+    # test the decks
+    @test length(PokerDeck.make_deck()) == 52
+    @test length(PokerDeck.draw!(5, PokerDeck.make_deck())) == 5
 
+    # some hand and eval tests
+    hand_string = ["2h", "3h", "4h", "5h", "Ah"]
+    @test length(PokerCard.newCard.(hand_string)) == 5
+    hand_ints = PokerCard.newCard.(hand_string)
+    println(Evaluator.class_to_string(Evaluator.get_rank_class(Evaluator.evaluate(hand_ints))))
+
+    # seven cards from the deck and eval
+    hand_ints = PokerDeck.draw!(7, PokerDeck.make_deck())
+    @test typeof(PokerCard.int_to_pretty_str.(hand_ints)) == Array{String,1}
+    @test typeof(Evaluator.get_rank_class(Evaluator.evaluate(hand_ints))) == Int
+
+end
 
 # first we'll import our modules
