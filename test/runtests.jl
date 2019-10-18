5c291a7dac7db41a09a6448cc352dfdc3b2d9cb3
Higgin <Town>
2019-10-18 17:18:19 -0500

    style

--- test/runtests.jl
+++ test/runtests.jl
@@ -27,15 +27,9 @@ import Quads.Evaluator
     t = @elapsed for i in 1:1000
         hand_ints = PokerDeck.draw!(7, PokerDeck.make_deck())
         Evaluator.get_rank_class(Evaluator.evaluate(hand_ints))
-    end
+    end # elapsed time 
 
     # 1k draws and evals in under 1s
     @assert t < 1
 
-
-
-
-
-end
-
-# first we'll import our modules
+end # tests
